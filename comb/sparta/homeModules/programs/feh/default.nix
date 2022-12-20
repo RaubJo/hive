840ab8337782e9a724e1acd6d493ed1cb3783c56
Joseph Raub <josephraub98@gmail.com>
2022-12-20 12:56:47 -0600

    Fixed spelling of folder

--- comb/sparta/homeModules/programs/feh/default.nix
+++
@@ -1,5 +0,0 @@
-{ config, lib, pkgs, ... }:
-
-{
-  programs.feh = { enable = true; };
-}
--- comb/sparta/homeModules/programs/git/default.nix
+++
@@ -1,9 +0,0 @@
-{ config, lib, pkgs, ... }:
-
-{
-  programs.git = {
-    enable = true;
-    userName = "Joseph Raub";
-    userEmail = "josephraub98@gmail.com";
-  };
-}
