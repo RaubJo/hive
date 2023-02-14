642d97f3346e52eef4c475c32831263d4174d4b8
Joseph Raub <josephraub98@gmail.com>
2023-02-14 09:22:30 -0600

    Update

--- comb/smenos/nixosConfigurations/kouphizo/default.nix
+++ comb/smenos/nixosConfigurations/kouphizo/default.nix
@@ -14,6 +14,12 @@ let
       wireless.interfaces = [ "wlan0" ];
       nameservers = [ "192.168.1.5" "100.95.211.76" "1.1.1.1" "8.8.8.8" ];
     };
+    sound.enable = true;
+    hardware = {
+      enableRedistributableFirmware = lib.mkDefault true;
+      bluetooth.enable = true;
+      pulseaudio.enable = true;
+    };
     system.stateVersion = "23.05";
   };
 in rec {
