92d0be03d8a5f986f0ac5b7885eb7f6377702e60
Joseph Raub <josephraub98@gmail.com>
2023-02-15 11:25:45 -0600

    Commented more nvidia stuff

--- comb/smenos/nixosProfiles.nix
+++ comb/smenos/nixosProfiles.nix
@@ -178,7 +178,7 @@ in {
   };
 
   sway = { pkgs, ... }: {
-    services.xserver.videoDrivers = [ "nvidia" ];
+    #services.xserver.videoDrivers = [ "nvidia" ];
     programs.sway = {
       enable = true;
       wrapperFeatures.gtk = true;
