4c4a6517a0d06b70e5331a6fc3a63be8eb5c0652
Joseph Raub <josephraub98@gmail.com>
2023-02-15 10:33:05 -0600

    Switched to awesomeWM

--- comb/smenos/nixosConfigurations/kouphizo/default.nix
+++ comb/smenos/nixosConfigurations/kouphizo/default.nix
@@ -5,7 +5,7 @@ let
       cell.hardwareProfiles.kouphizo
       cell.nixosSuites.base
       cell.nixosSuites.laptop
-      cell.nixosSuites.xmonad
+      cell.nixosSuites.awesome
     ];
     boot.kernelPackages = pkgs.linuxPackages_latest;
     networking = {
@@ -33,7 +33,7 @@ in rec {
     home-manager.useUserPackages = true;
     home-manager.useGlobalPkgs = true;
     home-manager.users.joseph = {
-      imports = [ ] ++ cell.homeSuites.joseph;
+      imports = [ ] ++ cell.homeSuites.josephAwesome;
       home.stateVersion = "23.05";
     };
   }];
